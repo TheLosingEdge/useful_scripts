3e772ff9f97877f702aa1bac815a298fc1dd8d28
TheL0singEdge <32302028+TheLosingEdge@users.noreply.github.com>
2020-08-31 10:31:01 -0500

    Update amass.sh

--- amass.sh
+++ amass.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 # $1 => example.domain
+# Requires https://github.com/tomnomnom/hacks/tree/master/filter-resolved
 
 amass enum --passive -d $1 -o domains_$1
 assetfinder --subs-only $1 | tee -a domains_$1
