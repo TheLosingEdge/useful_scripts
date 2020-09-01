bdaabe58c6c21e44e7fb18beddb8d4ff1d63aa17
TheL0singEdge <32302028+TheLosingEdge@users.noreply.github.com>
2020-09-01 14:03:11 -0500

    Update amass.sh

--- amass.sh
+++ amass.sh
@@ -12,7 +12,7 @@ subfinder -d $1 -o domains_subfinder_$1
 cat domains_subfinder_$1 | tee -a domains_$1
 
 #Project Sonar
-curl -s https://dns.bufferover.run/dns?q=.$1 | jq -r .FDNS_A[] | cut -d',' -f2| sort -u | jq -r .FDNS_A[] | cut -d',' -f2 | sort -u | tee -a domains_$1
+curl -s https://dns.bufferover.run/dns?q=.$1 | jq -r .FDNS_A[] | cut -d',' -f2 | sort -u | tee -a domains_$1
 
 #Filtering the domains
 sort -u domains_$1 -o domains_$1
